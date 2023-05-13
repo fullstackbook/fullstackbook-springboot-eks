c91409ba6d5842b705116c37e0815461392e2d46
Travis Luong <travis@travisluong.com>
2023-05-13 15:25:23 -0700

    initial commit

---
+++ src/main/java/com/example/fullstackbookspringbooteks/FullstackbookSpringbootEksApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackbookspringbooteks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackbookSpringbootEksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackbookSpringbootEksApplication.class, args);
+    }
+
+}
